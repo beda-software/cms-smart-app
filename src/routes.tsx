63eb25de29c94a5fe5bea5c1d19160763634495b
GenaRazmakhnin <gena.razmakhnin@gmail.com>
2021-03-18 20:23:45 +0800

    fix reload and image

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Navigate, PartialRouteObject } from "react-router";
 import { useStore } from "effector-react";
-import { $token } from "./stores/auth";
+import { $token, $client } from "./stores/auth";
 import SignIn from "./pages/auth/Signin";
 import EobDetail from "./pages/eob-detail";
 import PatientRecord from "./components/PatientRecord";
@@ -9,12 +9,31 @@ import Layout from "./pages/layout";
 
 const Routes = (): PartialRouteObject[] => {
   const token = useStore($token);
+  const client = useStore($client);
+
   if (!token) {
     return [
       {
         path: "/",
         element: <SignIn />,
       },
+      { path: "*", element: <Navigate to="/" /> },
+    ];
+  }
+
+  if (token && !client) {
+    return [
+      {
+        path: "/",
+        element: <Layout />,
+        children: [
+          {
+            path: "/",
+            element: <PatientRecord />,
+          },
+        ],
+      },
+
       { path: "*", element: <Navigate to="/" /> },
     ];
   }
